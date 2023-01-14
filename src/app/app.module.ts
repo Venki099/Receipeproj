5bba863c651dffaad3c65882204ed207d83bd53b
Venki099 <venkatesh.s@datalyzer.com>
2023-01-14 21:00:26 +0530

    Recipe

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,24 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { RecipesComponent } from './recipes/recipes.component';
+import { ReceipeListComponent } from './recipes/receipe-list/receipe-list.component';
+import { ReceipeDetailComponent } from './recipes/receipe-detail/receipe-detail.component';
+import { RecipeItemComponent } from './recipes/receipe-list/recipe-item/recipe-item.component';
+import { ShoppingListComponent } from './shopping-list/shopping-list.component';
+import { ShoppingEditComponent } from './shopping-list/shopping-edit/shopping-edit.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    RecipesComponent,
+    ReceipeListComponent,
+    ReceipeDetailComponent,
+    RecipeItemComponent,
+    ShoppingListComponent,
+    ShoppingEditComponent
   ],
   imports: [
     BrowserModule
---
+++ src/app/recipes/receipe-list/receipe-list.component.ts
@@ -0,0 +1,19 @@
+import { Component, OnInit } from '@angular/core';
+import { Recipe } from '../recipe.model';
+
+@Component({
+  selector: 'app-receipe-list',
+  templateUrl: './receipe-list.component.html',
+  styleUrls: ['./receipe-list.component.css']
+})
+export class ReceipeListComponent implements OnInit {
+recipes:Recipe[] = [
+  new Recipe('A test recipe','This is test','https://www.themediterraneandish.com/wp-content/uploads/2020/02/falafel-recipe-1.jpg'),
+  new Recipe('A test recipe','This is test','https://sweetbakedlife.files.wordpress.com/2014/09/cablecar.jpg')
+];
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
